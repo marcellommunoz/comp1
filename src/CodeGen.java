4ac9342129926a1ea710b8c69425ad0e304e4858
Wagner <wagner_loch@hotmail.com>
2018-09-09 13:32:04 -0300

    CodeGen criando arquivo

--- src/CodeGen.java
+++ src/CodeGen.java
@@ -1,33 +1,47 @@
+import java.io.FileWriter;
+import java.io.PrintWriter;
+
 class CodeGen{
 
 	
-	String geraCodigo (ArvoreSintatica arv)
+	
+	String geraCodigo (ArvoreSintatica arv) throws java.io.IOException
 	{
-		return (geraCodigo2(arv) + "PRINT");
+		String textoArquivo = "";
+		FileWriter arquivo = new FileWriter("saidaCodeGen.txt");
+		PrintWriter gravarArquivo = new PrintWriter(arquivo);
+		textoArquivo = geraCodigo2(arv) + "PRINT";
+		gravarArquivo.print(textoArquivo);
+		arquivo.close();
+		return (textoArquivo);
 	}
-	String geraCodigo2 (ArvoreSintatica arv)
+	String geraCodigo2 (ArvoreSintatica arv) throws java.io.IOException
 	{
+		if (arv instanceof Mult) {
+			return (geraCodigo2(((Mult) arv).arg1) + 
+				geraCodigo2(((Mult) arv).arg2) +
+				"MULT\r\n");
+		}
 
-	if (arv instanceof Mult)
-		return (geraCodigo2(((Mult) arv).arg1) + 
-			geraCodigo2(((Mult) arv).arg2) +
-			"MULT\n");
+		if (arv instanceof Soma) {
+			return (geraCodigo2(((Soma) arv).arg1) + 
+				geraCodigo2(((Soma) arv).arg2) +
+				"SUM\r\n");
+		}
+		if (arv instanceof Sub) {
+			return (geraCodigo2(((Sub) arv).arg1) + 
+				geraCodigo2(((Sub) arv).arg2) +
+				"SUB\r\n");
+		}
+		if (arv instanceof Div) {
+			return (geraCodigo2(((Div) arv).arg1) + 
+				geraCodigo2(((Div) arv).arg2) +
+				"DIV\r\n");
+		}
+		if (arv instanceof Num) {
+			return ("PUSH "  + ((Num) arv).num + "\r\n");
+		}
 
-	if (arv instanceof Soma)
-		return (geraCodigo2(((Soma) arv).arg1) + 
-			geraCodigo2(((Soma) arv).arg2) +
-			"SUM\n");
-	if (arv instanceof Sub)
-		return (geraCodigo2(((Sub) arv).arg1) + 
-			geraCodigo2(((Sub) arv).arg2) +
-			"SUB\n");
-	if (arv instanceof Div)
-		return (geraCodigo2(((Div) arv).arg1) + 
-			geraCodigo2(((Div) arv).arg2) +
-			"DIV\n");
-	if (arv instanceof Num)
-		return ("PUSH "  + ((Num) arv).num + "\n");
-
-	return "";
+		return "";
 	}
 }
